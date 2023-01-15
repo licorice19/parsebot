37f5934e022313e66e2f61f5844fbab763aedb65
Dima <dkozyarskij@gmail.com>
2023-01-15 18:49:54 +0500

    код переработан

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+from time import sleep
+import re
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://www.avito.ru/novotroitsk/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw"
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 'accept': '*/*'}
+URL = 'https://www.avito.ru/novotroitsk/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw'
+PAUSE_DURATION_SECONDS = 5
+CLEANR = re.compile('<.*?>')
 
 
-def get_html(url, params=None) -> requests.Response:
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
+def main():
+    driver.get(URL)
+    sleep(PAUSE_DURATION_SECONDS)
+    with open("page.html", "w", encoding="utf-8") as file:
+        file.write(driver.page_source)
+    driver.quit()
+    get_items("page.html")
 
-soup = BeautifulSoup(get_html(URL).text, "html.parser")
 
-print(soup.find_all("h3", class_="title-root-zZCwT"))
+def get_items(file):
+    with open(file, "r", encoding="utf-8") as file:
+        html = file.read()
+    soup = BeautifulSoup(html, "lxml")
+    items = soup.find_all("div", attrs={"data-marker": "item"})
+    goods = []
+    for index, item in enumerate(items):
+        item_url = item.find('a').get("href")
+        item_price = item.find("span", attrs={"data-marker": "item-price"}).find('meta',attrs={"itemprop": "price"}).get('content')
+        item_title = item.find("h3", attrs={"itemprop": "name"}).contents[0]
+        goods.append(f"{index} @title {item_title} @price {item_price} @url https://avito.ru{item_url}")
+
+    with open("urls.txt", "w", encoding="utf-8") as file:
+        for good in goods:
+            file.write(f"{good} \n")
+
+
+if __name__ == '__main__':
+    try:
+        service = Service(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+        main()
+    except Exception as e:
+        print(e)
+    finally:
+        pass

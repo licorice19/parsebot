2b66e53646e7a7664b58806831c1c7e85b4246f8
Dmitry Kozyarskij <dkozyarskij@gmail.com>
2023-01-23 19:36:09 +0500

    колосс на глиняных ногах

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 from time import sleep
-import re
+import aiofiles
+import json
 import os
 import asyncio
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import mainbot as mybot
+# from telebot.async_telebot import AsyncTeleBot
+from telebot.async_telebot import AsyncTeleBot
+from parser_module import get_items, filter_by_city, diff_search
 
 URL = 'https://www.avito.ru/novotroitsk/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw'
 PAUSE_DURATION_SECONDS = 5
@@ -15,102 +17,161 @@ CITIES = ['novotroitsk', 'orenburg', 'orsk']
 INTERVAL = 3600
 CATEGORIES = []
 
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
+PASSWORD = os.environ.get("PASSWORD")
 
+# bot = AsyncTeleBot(TOKEN)
+
+TOKEN = os.environ.get("TOKEN")
+PASSWORD = os.environ.get("PASSWORD")
+
+bot = AsyncTeleBot(TOKEN)
+
+# Dictionary to keep track of active users and their current states
+try:
+    with open("users.json", "r") as f:
+        users = json.load(f)
+except FileNotFoundError:
+    users = {}
+
+@bot.message_handler(commands=['start'])
+async def startup_function(message):
+    chat_id = message.chat.id
+    # Add the user to the dictionary with an initial state of 'inactive'
+    if not users[chat_id]:
+        users[chat_id] = {"state": "inactive", "data": {}}
+    welcome_message = f"Welcome! To get started, type '/start_parse' to start the parsing function. Type '/stop' to stop the function. Type '/help' to display all commands"
+    await bot.send_message(chat_id, welcome_message)
+    async with aiofiles.open("users.json", "w") as f:
+        await f.write(json.dumps(users))
+        
+@bot.message_handler(commands=['help'])
+async def help_function(message):
+    chat_id = message.chat.id
+    help_message = f"Commands available: \n /start_parse: starts the parsing function \n /stop: stops the function"
+    await bot.send_message(chat_id, help_message)
+
+# Function to handle the start command
+@bot.message_handler(commands=['start_parse'])
+async def start_parser(message):
+    chat_id = message.chat.id
+    if users[chat_id]["state"] == "inactive":
+        if users[chat_id]["data"]["city"]:
+            city = users[chat_id]["data"]["city"]
+            users[chat_id]["state"] = "active"
+            await bot.send_message(chat_id, "Parser started for city: {}. Type '/stop_parser' to end the function.".format(CITIES[city]))
+
+            while users[chat_id]["state"] == "active":
+                await parser(chat_id, city)
+                await asyncio.sleep(30 * 60)
+    else:
+        await bot.send_message(chat_id, "Parser is started")
+        
+@bot.message_handler(commands=['get_data'])
+async def get_data(message):
+    chat_id = message.chat.id
+    path_file = f"users\\{chat_id}\\diff.json"
+    try:
+        with open("f.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        await bot.send_message(chat_id, "Data file not found.")
+        return
+    pages = paginate_data(data)
+    current_page = 0
+    while True:
+        current_data = pages[current_page]
+        message = ""
+        for item in current_data:
+            message += f"Title: {item['title']}\nPrice: {item['price']}\nURL: {item['url']}\n\n"
+        await bot.send_message(chat_id, message)
+        if current_page == len(pages) - 1:
+            break
+        current_page += 1
+        await asyncio.sleep(30)
+
+@bot.message_handler(commands=['city'])
+async def city_command(message):
+    # Get the chat ID of the user
+    chat_id = message.chat.id
+    # Get the argument passed with the command
+    city = message.text.split(" ")[1]
+    # Update the user's data in the dictionary
+    users[chat_id]["data"]["city"] = city
+    # Notify the user that the city has been set
+    await bot.send_message(chat_id, f"City set to {CITIES[city]}.")
+    with aiofiles.open("users.json", "w") as f:
+        json.dump(users, f)
+
+# Function to handle the stop command
+@bot.message_handler(commands=['stop'])
+async def stop_function(message):
+    chat_id = message.chat.id
+    if chat_id in users:
+        # Remove the user from the dictionary
+        del users[chat_id]
+        await bot.send_message(chat_id, "Function stopped.")
+        async with aiofiles.open("users.json", "w") as f:
+            json.dump(users, f)
+    else:
+        await bot.send_message(chat_id, "Function is not running.")
+
+def paginate_data(data, items_per_page=1):
+    pages = []
+    current_page = []
+    for item in data.values():
+        current_page.append(item)
+        if len(current_page) == items_per_page:
+            pages.append(current_page)
+            current_page = []
+    if current_page:
+        pages.append(current_page)
+    return pages
+
+
+## Точка входа
 def main():
-    parser(-1, 0)
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.polling())
+    loop.run_forever()
 
 # Основная функция парсинга
-def parser(chat_id, city):
+async def parser(chat_id, city):
+    #пути
     path = f"users\\{chat_id}\\"
+    src_page = f"{path}page.html"
+    path_urls = f"{path}urls.json"
+    old_path_urls = f"{path}urls_old.json"
+    filtered_urls = f"{path}f.json"
+    old_filtered_urls = f"{path}f_old.json"
 
+    #загрузка вебдрайвера
     op = webdriver.ChromeOptions()
     op.add_argument('--headless')
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=op)
     driver.get(URL)
+    
     if not os.path.exists(path):
         os.makedirs(path)
 
     sleep(PAUSE_DURATION_SECONDS)
-    with open(f"{path}page.html", "w", encoding="utf-8") as file:
+    # сохранение страницы
+    with open(src_page, "w", encoding="utf-8") as file:
         file.write(driver.page_source)
 
-    get_items(f"{path}page.html", path=path)
-    filter_by_city(city, f"{path}urls.txt", path)
-    diff_search(f"{path}f.txt", f"{path}f_old.txt", path)
+    # Основной парсинг
+    get_items(src_page, path=path)
+    filter_by_city(city, path_urls, path)
+    diff_search(filtered_urls, old_filtered_urls, path)
 
+    # удаление страницы
     if os.path.exists(f"{path}page.html"):
         os.remove(f"{path}page.html")
+
     driver.close()
     driver.quit()
-
-def get_items(file, path): 
-    urls_file = f"{path}urls.txt"
-    urls_old_file = f"{path}urls_old.txt"
-
-    if os.path.exists(urls_file):
-        if os.path.exists(urls_old_file):
-            os.remove(urls_old_file)
-        os.rename(urls_file, urls_old_file)
-
-    with open(file, "r", encoding="utf-8") as file:
-        html = file.read()
-
-    soup = BeautifulSoup(html, "lxml")
-    items = soup.find_all("div", attrs={"data-marker": "item"})
-    goods = []
-
-    for item in items:
-        item_url = item.find('a').get("href")
-        item_price = item.find("span", attrs={"data-marker": "item-price"}).find('meta', attrs={"itemprop": "price"}).get('content')
-        item_title = item.find("h3", attrs={"itemprop": "name"}).contents[0]
-        goods.append(f"@title {item_title} @price {item_price} @url https://avito.ru{item_url}")
-
-    with open(urls_file, "w", encoding="utf-8") as file:
-        for good in goods:
-            file.write(f"{good} \n")
-
-# фильтрация текстового файла по городу
-def filter_by_city(city, file, path):
-    items = []
-    f_file = f"{path}f.txt"
-    f_old_file = f"{path}f_old.txt"
-
-    if os.path.exists(f_file):
-        if os.path.exists(f_old_file):
-            os.remove(f_old_file)
-        os.rename(f_file, f_old_file)
-
-    with open(file, "r", encoding="utf-8") as file:
-        while True:
-            line = file.readline()
-            if re.findall(CITIES[city], line):
-                items.append(line)
-            if not line:
-                write_list_in_file(items, f"{path}f.txt")
-                break
-
-# запись листа в файл
-def write_list_in_file(list, filename):
-    with open(filename, "w", encoding="utf-8") as file:
-        for item in list:
-            file.write(f"{item}")
-
-# Выборка различающихся строк из двух файлов
-def diff_search(file1, file2, path):
-    file = f"{path}diff.txt"
-    if os.path.exists(file1):
-        if os.path.exists(file2):
-            f1 = open(file1, "r", encoding="utf-8")
-            f2 = open(file2, "r", encoding="utf-8")
-            list1 = f1.readlines()
-            list2 = f2.readlines()
-            diff_lines = list(set(list1) - set(list2))
-            if os.path.exists(file):
-                os.remove(file)
-            if diff_lines:
-                write_list_in_file(diff_lines, file)
-
 
 # Точка входа
 if __name__ == '__main__':
---
+++ parser_module.py
@@ -0,0 +1,102 @@
+import os
+from bs4 import BeautifulSoup
+import json
+from collections import OrderedDict
+
+def get_items(file, path): 
+    urls_file = f"{path}urls.json"
+    urls_old_file = f"{path}urls_old.json"
+
+    if os.path.exists(urls_file):
+        if os.path.exists(urls_old_file):
+            os.remove(urls_old_file)
+        os.rename(urls_file, urls_old_file)
+
+    with open(file, "r", encoding="utf-8") as file:
+        html = file.read()
+
+    soup = BeautifulSoup(html, "lxml")
+    items = soup.find_all("div", attrs={"data-marker": "item"})
+    goods = {}
+
+    for index, item in enumerate(items):
+        item_url = item.find('a').get("href")
+        item_price = item.find("span", attrs={"data-marker": "item-price"}).find('meta', attrs={"itemprop": "price"}).get('content')
+        item_title = item.find("h3", attrs={"itemprop": "name"}).contents[0]
+        goods[index] = {'title':item_title, 'price':item_price, 'url': f"https://avito.ru{item_url}"}
+    with open(urls_file, "w", encoding="utf-8") as file:
+        goods = OrderedDict(sorted(goods.items()))
+        json.dump(goods, file)
+
+# фильтрация текстового файла по городу
+def filter_by_city(city, file_path, output_path):
+    city_code = CITIES[city]
+    filtered_items = {}
+
+    # Rename the previous filtered file to a backup
+    filtered_file = f"{output_path}f.json"
+    old_filtered_file = f"{output_path}f_old.json"
+    if os.path.exists(filtered_file):
+        if os.path.exists(old_filtered_file):
+            os.remove(old_filtered_file)
+        os.rename(filtered_file, old_filtered_file)
+
+    # Open and read the input file
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        
+        # Iterate through the data and filter by city
+        for key, value in data.items():
+            if city_code in value['url']:
+                filtered_items[key] = value
+
+    # Sort the filtered items
+    filtered_items = OrderedDict(sorted(filtered_items.items()))
+
+    # Write the filtered items to a new file
+    filtered_file = f"{output_path}/f.json"
+    with open(filtered_file, "w", encoding="utf-8") as file:
+        json.dump(filtered_items, file)
+
+def json_compare(json1, json2):
+    try:
+        f1 = open(json1, "r", encoding="utf-8")
+        f2 = open(json2, "r", encoding="utf-8")
+        dict1 = json.load(f1)
+        dict2 = json.load(f2)
+        if dict1 and dict2:
+            diff =  [x for x in dict1.values() if x not in dict2.values()] + [x for x in dict2.values() if x not in dict1.values()]
+            print(diff)
+        else:
+            print("The dict1 or dict2 are empty")
+    except FileNotFoundError as e:
+        print(f"{e} : Invalid file path")
+    except json.decoder.JSONDecodeError as e:
+        print(f"{e} : Invalid json format")
+    except Exception as e:
+        print(e)
+    finally:
+        f1.close()
+        f2.close()
+
+# Выборка различающихся строк из двух файлов
+def diff_search(file1, file2, path):
+    diff_file = f"{path}diff.json"
+    try:
+        if os.path.exists(file1) and os.path.exists(file2):
+            with open(file1, "r", encoding="utf-8") as f1, open(file2, "r", encoding="utf-8") as f2:
+                dict1 = json.load(f1)
+                dict2 = json.load(f2)
+                diff =  [x for x in dict1.values() if x not in dict2.values()] + [x for x in dict2.values() if x not in dict1.values()]
+                if diff:
+                    with open(diff_file, "w", encoding="utf-8") as file:
+                        json.dump(diff, file)
+    except FileNotFoundError as e:
+        print(f"{e} : Invalid file path")
+    except json.decoder.JSONDecodeError as e:
+        print(f"{e} : Invalid json format")
+    except Exception as e:
+        print(e)
+    finally:
+        f1.close()
+        f2.close()
